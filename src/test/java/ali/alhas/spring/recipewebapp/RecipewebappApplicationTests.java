ab7557fae99658cb946dc478f53dfd8c6a6c2d0c
Bahtiyar Ali ALHAS <bahtiyaralialhas@gmail.com>
2020-02-16 19:04:28 +0100

    first commit

---
+++ src/test/java/ali/alhas/spring/recipewebapp/RecipewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.alhas.spring.recipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipewebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
