420aae112a8eeefa36a25966d1023b313fa3ea9d
Bahtiyar Ali ALHAS <bahtiyaralialhas@gmail.com>
2020-02-16 23:02:22 +0100

    Creating Index Page Closes. #1

---
+++ src/main/java/ali/alhas/spring/controllers/indexController.java
@@ -0,0 +1,15 @@
+package ali.alhas.spring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"","/","index"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
